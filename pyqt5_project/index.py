2e072eef2c90df28a4032fb4fa7a52ef39ddcab1
Hyewon Jeong <55379636+hyemWon@users.noreply.github.com>
2021-10-07 18:48:13 +0900

    Add files via upload

---
+++ pyqt5_project/index.py
@@ -0,0 +1,14 @@
+# 관리자용 cctv과제 프로그램 (PyQt5 - SQLite - Socket)
+# 1) 사원 회원가입
+# 2) 사원 로그인
+# 3) 전체 사원들과의 채팅
+# 4) cctv 관제
+# 5) cctv 1개 클릭시 -> yolo detection 수행 화면 띄우기
+
+import sys
+# from PyQt5.uic import loadUi
+from PyQt5 import uic
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+
+
